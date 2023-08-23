e78cfb3678ddfb73505ecb413df7f45344df1131
EdoRguez <edro998@gmail.com>
2023-08-23 17:59:55 -0400

    GORM tables creation

--- db/connection.go
+++ db/connection.go
@@ -3,22 +3,25 @@ package db
 import (
 	"log"
 
+	"github.com/edorguez/go-api/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 // var DSN = "host=localhost user=eduardo password=mysecretpassword dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 var DSN = "host=localhost user=postgres password=admin dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-
-// var DSN = "postgres://eduardo:mysecretpassword@localhost:5432/gorm"
-var DB *gorm.DB
 
 func DBConnection() {
 	var error error
 	DB, error := gorm.Open(postgres.Open(DSN), &gorm.Config{})
-	if error != nil {
+	if error != nil || DB == nil {
 		log.Fatal(error)
 	} else {
-		log.Println("DB conntected to ", DB)
+		log.Println("DB conntected to ")
+
+		DB.AutoMigrate(models.Task{})
+		DB.AutoMigrate(models.User{})
 	}
 }
